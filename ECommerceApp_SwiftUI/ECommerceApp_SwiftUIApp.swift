8cbc0047943d78d97620ed8212581e57707711f4
Nagaraju0323 <nagarajukios85@gmail.com>
2023-12-16 20:46:52 +0530

    firstcommit

---
+++ ECommerceApp_SwiftUI/ECommerceApp_SwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  ECommerceApp_SwiftUIApp.swift
+//  ECommerceApp_SwiftUI
+//
+//  Created by Nagaraju on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceApp_SwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
